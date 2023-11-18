4c160c05a7964a7842cb0a7404ff003ada302e77
unknown <andrewtsegaye7@gmail.com>
2023-11-18 13:30:04 +0300

    fully updated

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import logo from './logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import python from './tech/python.png';
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import jupyter from './tech/jupyter.png';
+
+import addiscoder from './company/addiscoder.png';
+import robox from './company/robox.png';
+import freelancer from './company/freelancer.png';
+import jsm from './company/jsm.png';
+
+import georg from './testimonials/georg.jpg';
+import isaac from './testimonials/isaac.jpg';
+import mukerem from './testimonials/mukerem.png';
+
+import adventure from './adventure.jpg';
+import flappybird from './flappybird.jpg';
+import pong from './pong.jpg';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  adventure,
+  flappybird,
+  pong,
+  addiscoder,
+  robox,
+  freelancer,
+  jsm,
+  python,
+  jupyter,
+  github,
+  menu,
+  close,
+  figma,
+  git,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  georg,
+  isaac,
+  mukerem,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,272 @@
+import {
+  python,
+  jupyter,
+  javascript,
+  typescript,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  threejs,
+  mobile,
+  backend,
+  creator,
+  web,
+  adventure,
+  flappybird,
+  pong,
+  addiscoder,
+  robox,
+  freelancer,
+  jsm,
+  georg,
+  isaac,
+  mukerem,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Python Programming',
+    icon: web,
+  },
+  {
+    title: 'Algorithms and Models',
+    icon: mobile,
+  },
+  {
+    title: 'Full Stack Development',
+    icon: backend,
+  },
+  {
+    title: 'Blockchain Development',
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: 'Python',
+    icon: python,
+  },
+  {
+    name: 'Jupyter',
+    icon: jupyter,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Robotics Workshop',
+    company_name: 'Robox Robotics And Artificial Intelligence Center',
+    icon: robox,
+    iconBg: '#E6DEDD',
+    date: 'July 2022 - Aug 2022',
+    points: [
+      'Leader of a research team that created and manipulated a robot through Arduino programming which cleans and paints the chimney of a thermal power plant.',
+      'Conducting robotics workshop for junior students.',
+      'Planning a monthly schedule of team operation.',
+      'Building and assembling robots as part of hands-on sessions.',
+    ],
+  },
+  {
+    title: 'Creative Designer & Developer',
+    company_name: 'JavaScript Mastery',
+    icon: jsm,
+    iconBg: '#E6DEDD',
+    date: 'Aug 2022 - July 2023',
+    points: [
+      'Worked in modern web technologies (React.js, Next.js, GraphQL, Node.js)',
+      "Contributed to defining best practices for reusable UI components in the company's design system.",
+      'Collaborated with cross-functional teams, including designers and backend engineers, to implement new features and functionality.',
+      "Managed the frontend of the company's web platform, ensuring a seamless and user-friendly experience.",
+    ],
+  },
+  {
+    title: 'Algorithms & Programming',
+    company_name: 'AddisCoder',
+    icon: addiscoder,
+    iconBg: '#383E56',
+    date: 'July 2023 - Aug 2023',
+    points: [
+      'Attended AddisCoder, a top-tier summer program for high-achieving students in Ethiopia.',
+      'Received intensive instruction in Python, algorithm design, and advanced computer science topics.',
+      'Participated in hands-on coding labs and problem-solving.',
+      'Joined a network of successful AddisCoder alumni.',
+    ],
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Freelancer',
+    icon: freelancer,
+    iconBg: '#383E56',
+    date: 'Mar 2021 - Present',
+    points: [
+      'Designed and developed responsive web and mobile applications with user-friendly interfaces.',
+      'Collaborated with clients to define project goals and requirements.',
+      'Managed databases and ensured data efficiency.',
+      'Conducted testing, debugging, and deployed applications to production environments.',
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      'Andrew Tsegaye, a skilled Python and JavaScript programmer, excels in technical collaboration. His enthusiasm and growth potential promise positive contributions to future projects.',
+    name: 'Mukerem Ali',
+    designation: 'Python Developer',
+    company: 'Turing',
+    image: mukerem,
+  },
+  {
+    testimonial:
+      'Andrew shows promising tenacity, dedication, and problem-solving skills. His eagerness to learn and take initiative positions him for success in software engineering. I anticipate his future achievements.',
+    name: 'Isaac Sahle',
+    designation: 'Software Engineer',
+    company: 'Google',
+    image: isaac,
+  },
+  {
+    testimonial:
+      "Andrew, a tech explorer with diverse GitHub projects, excels in AI and app dev't. He leveraged experience, embraced challenges, and eagerly sought improvement, demonstrating a keen learning spirit.",
+    name: 'Georg Osang',
+    designation: 'Mathematical Scientist',
+    company: 'IDEMS',
+    image: georg,
+  },
+];
+
+const projects = [
+  {
+    name: 'AI Plays Ping Pong',
+    description:
+      "Developed a reinforcement learning AI agent for Pong, controlling a paddle to block the ball's progress.",
+    tags: [
+      {
+        name: 'Python',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Reinforcement Learning',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'NEAT',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: pong,
+    source_code_link:
+      'https://github.com/Andrew-Tsegaye/AI-Playing-Pong-Game',
+  },
+  {
+    name: 'AI Adventure',
+    description:
+      'Crafted an AI-driven Python Choose-Your-Own-Adventure game, leveraging OpenAI, Astra vector database, and RAG technology.',
+    tags: [
+      {
+        name: 'Python',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Vector DB',
+        color: 'orange-text-gradient',
+      },
+      {
+        name: 'OpenAI',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'RAG',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: adventure,
+    source_code_link: 'https://github.com/Andrew-Tsegaye/AI-Choose-Your-Own-Adventure-Game',
+  },
+  {
+    name: 'AI Plays Flappy Bird',
+    description:
+      'Implemented an AI algorithm using NEAT to play Flappy Bird, showcasing neural network evolution for bird behavior control.',
+    tags: [
+      {
+        name: 'Python',
+        color: 'pink-text-gradient',
+      },
+      {
+        name: 'Algorithm',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'NEAT',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Pygame',
+        color: 'orange-text-gradient',
+      },
+    ],
+    image: flappybird,
+    source_code_link: 'https://github.com/Andrew-Tsegaye/AI-Powered-Flappy-Bird',
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
